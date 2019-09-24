319f8991a00e28a556723ad3209a6a2eaf91a6f5
Greg <gschwartzenager@gmail.com>
2019-09-24 15:09:55 -0500

    Hide API key

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import './App.css'
 import Calendar from 'react_google_calendar'
+import calendar_config from './calendar_config'
 
-const calendar_configuration = {
-  api_key: 'AIzaSyDEoqeQyrQ2HZaA7FUzYWfJEu8fXU70PAs',
-  calendars: [
-      {
-        name: 'Green House Calendar', // whatever you want to name it
-        url: 'qmgl5raib3ehhmqbov7fu0k99g@group.calendar.google.com' // your calendar URL
-      }
-    ]
-}
+const calendar_configuration = calendar_config
 
 class App extends React.Component {
   constructor (props) {
