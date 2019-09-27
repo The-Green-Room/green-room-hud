457aca3860618d702be1cf6942e5e5d261ce8ce8
Greg <gschwartzenager@gmail.com>
2019-09-26 23:48:36 -0500

    Got quotes and authors on screen

--- src/App.js
+++ src/App.js
@@ -8,20 +8,30 @@ import {
   Col
 } from 'react-bootstrap'
 import Clock from 'react-clock'
+import quoteStrings from './quotes'
 
 const calendar_configuration = calendar_config
 
 class App extends React.Component {
   constructor (props) {
     super (props)
+
     this.state = {
       events: [],
-      time: new Date()
+      time: new Date(),
+      quote: '',
+      author: ''
     }
   }
 
   componentDidMount () {
-    this.interalID = setInterval(() => this.tick(), 1000)
+    this.getNewQuote()
+    this.interalID = setInterval(() => {
+      this.tick()
+      if ((this.state.time.getMinutes() % 15 === 0) && (this.state.time.getSeconds() % 60 === 0)) {
+        this.getNewQuote()
+      }
+    }, 1000)
   }
 
   componentWillUnmount () {
@@ -32,17 +42,17 @@ class App extends React.Component {
     this.setState({ ...this.state, time: new Date() })
   }
 
+  getNewQuote () {
+    const fullQuoteText = quoteStrings[Math.floor(Math.random() * 100)]
+    const authorSlice = fullQuoteText.slice(fullQuoteText.search(/ -\w[A-Za-z .\-!?]*/))
+    const quoteSlice = fullQuoteText.slice(0, fullQuoteText.search(/ -\w[A-Za-z .\-!?]*/))
+    this.setState({ ...this.state, quote: quoteSlice, author: authorSlice})
+  }
+
   render () {
     let h
     const currentTime = this.state.time
     const bigTimeString = `${(h = currentTime.getHours()) === 0 ? 12 : h < 12 ? h : h - 12}:${currentTime.getMinutes().toString().padStart(2, '0')} ${(h < 12 ? 'AM' : 'PM')}`
-    const testQuoteString =
-    <div>
-      <p> I'm loving this canned wine thing. </p>
-        <p> It's brilliant! </p>
-          <p> I'm active! </p>
-            <p> I'm gesturing with my hands! </p>
-    </div>
 
     return (
       <div>
@@ -57,11 +67,17 @@ class App extends React.Component {
           rel='stylesheet'
           type='text/css'
         />
+        <link
+          href='https://fonts.googleapis.com/css?family=Lobster&display=swap'
+          rel='stylesheet'
+          type='text/css'
+        />
 
         <Container fluid={true} style={{ flexWrap: 'nowrap' }}>
           <Row style={{ height: 960 }}>
-            <Col style={{ flexGrow: 1, alignSelf: 'center', justifyContent: 'center' }}>
-              {testQuoteString}
+            <Col style={{ flexGrow: 1, alignSelf: 'center', fontFamily: 'Lobster' }}>
+              <p style={{ fontSize: 40 }}>{this.state.quote}</p>
+              <p className='text-right' style={{ fontSize: 24 }}>{this.state.author}</p>
             </Col>
             <Col style={{
               alignSelf: 'center',
---
+++ src/quotes.js
@@ -0,0 +1,201 @@
+export default [
+  '[Singing] You gotta pay the troll toll, if you wanna get into that boy’s soul. -Frank',
+
+  'So you think not eating cat food is putting on airs? -Mac',
+
+  'We’re trying to give you the Christmas spirit, dickhole! -Sweet Dee',
+
+  'Yippie-kay-yay, Mr. Falcon! -Frank',
+
+  'Charlie, make a move. Just tell her you wanna bang her. -Mac',
+
+  'Mac, you have an exceptional number of bugs in your teeth. You’re gonna wanna rinse those out. It’s disgusting. -Dennis',
+
+  'Grain alcohol, baby! Whenever there’s a potential riot, I’m getting blasted on grain alcohol. -Mac',
+
+  'Okay, Mr. Mayor. Feast your ears on that Spin Doctors mix. -Charlie',
+
+  'Dude, do you have a boner right now? -Dennis',
+
+  'Name’s Artemis. I have a bleached asshole. -Artemis',
+
+  'And you know what happens with Tokyo drifting? It leads to bickering. Which, of course, leads to karate. -Mac',
+
+  'The acid’s makin’ me feel like I gotta take a dump. -Frank',
+
+  'Dude, I swear to God, if you try and give me a noogie, I will yank your underwear over your head so hard your asshole will rip in half. -Dennis',
+
+  '[Holding a calculator] What are you? -Charlie',
+
+  'Any amount of cheese before a date is too much cheese. -Dennis',
+
+  'Taked baby. Meet at later bar, night or day sometime. -Sweet Dee',
+
+  'Smoke some cigarettes. The smoke will suffocate the bacteria in your stomach. -Mac',
+
+  'Yeah, well, you’ve never once seen me wash my testicles either, but that doesn’t mean I don’t do it every Friday. -Charlie',
+
+  'Animals should be food, rugs and trophies. Why do you think I’m wearing a leather suit? -Frank',
+
+  'Goddammit, Jack Bauer. You really are the man. -Dennis',
+
+  'Dennis, you don’t wanna go anywhere near a court. How many bench warrants for sexual misconduct do you have? Yeah, and Frank, how many unregistered guns are in your car right now? -Sweet Dee',
+
+  'Oh, look at me! The millionaire who goes to see doctors! -Charlie',
+
+  'I’m just saying, based on that story that you just told me, I’m fairly certain that those Santas were running a train on your mom for money. -Mac',
+
+  'Then start breakin’ bricks, wet nips! -Liam McPoyle',
+
+  'That’s Tammy, Trey’s ex-girlfriend. This is classic Tammy. Trey broke up with Tammy because Maureen Kinallen said that she saw Tammy flirting with Walt Timby at a party. But she was only doing it to make Trey jealous because, you know, she thought that Trey secretly liked Erin Hannabry. But Trey didn’t like Erin Hannabry. It was all a bunch of bull. -Charlie',
+
+  'RUM HAM! RUM HAM! I’m sorry rum ham! I’m sorry… -Frank',
+
+  'Mac, I’m gonna stop you right there. First of all, your breath smells like an old lady fart passing through an onion. Secondly, I know you’re trying to manipulate me, and it’s not going to work. Get your hand off my shoulder, because I’ve got a fatty to burn. -Sweet Dee',
+
+  'You’re a stone cold fox, Margaret. You’re a stone cold fox, and I want you. I gotta have you. I need you. I want you inside me. But you know that, don’t you, Margaret? -Dennis',
+
+  'Oh, I get it. Cute. You leave this pen here and people are supposed to think, “Wait, that looks like a dick.” -Mac',
+
+  'Well, I don’t know how many years on this Earth I got left. I’m gonna get real weird with it. -Frank',
+
+  'Take a look at Rex here. That is a body that just won’t quit. And I bet if you pop those pants off, you’re gonna find a bird that just won’t quit, either. And I think that’ll come in handy in this situation. I think the problem here is that your body quit. Your bird quit. And, unfortunately, it’s no longer legit. -Mac',
+
+  'Here’s a confession: I’m in love with a man. What? I’m in love with a man. A man called God. Does that make me gay? Am I gay for God? You betcha! -Charlie',
+
+  'Hi. Um, I’m a recovering crackhead. This is my retarded sister that I take care of. I’d like some welfare, please. -Dennis',
+
+  'I’m not asking you to do much. Just turn a blind eye while I rob this place stupid. -Sweet Dee',
+
+  '"How’s that ass feel? [slaps Dennis in the ass]" -Large Black Man in a Towel',
+
+  'Isn’t that amazing? You ask to see a woman’s breasts on the street, you get slapped. You give her a free t-shirt and videotape it, and the clothes come right off. -Mac',
+
+  '[In Boston accent] Ohhh, bet you read a lotta Gordon Wood, huh? You read your Gordon Wood and you regurgitate it from a textbook and you think you’re wicked awesome doin’ that, and how ’bout ‘dem apples? And all that Gordon Wood business. -Charlie',
+
+  'You know what, Dee? I don’t wanna hear about your dreams, okay? I hate listening to people’s dreams. It’s like flipping through a stack of old photographs. If I’m not in any of ’em and nobody’s having sex, I just- I don’t care. -Dennis',
+
+  'I mean, does my scar look like a dog’s vagina? You know, maybe. I don’t know. I’m not gonna sit here and try and get inside the mind of a dog. I mean, that’s God’s work. Well, not that I believe in God. I don’t. Not since that Chinaman stole my kidney. -Rickety Cricket',
+
+  'Then, I’ll just regress, because I feel I made myself perfectly redundant. -Charlie',
+
+  '[Reading Charlie’s script] “Hello fellow American. This you should vote me. I leave power. Good. Thank you. Thank you. If you vote me, I’m hot.” What? “Taxes. They’ll be lower. Son. The democratic vote for me is right thing to do, Philadelphia. So do.” -Dennis',
+
+  'Do not plug an open wound with trash. -Mac',
+
+  'It’s fetish-fetish shit! I like to bind. I like to BE bound! -Dennis',
+
+  'I’m gonna pinch their dicks with this lobster. -Frank',
+
+  'This bar runs on trash, dude. This bar is totally green that way. -Charlie',
+
+  'Well, maybe you’re a whore, you stupid, ugly, homeless bitch! -Sweet Dee',
+
+  'Okay, well, “Charlie work” is like, you know, like basement stuff, cleaning urinals, uh, blood stuff, your basic slimes, your sludges, anything dead or decaying. You know, I’m on it. I’m dealing with it. -Charlie',
+
+  'Bruce is not banging any baby dudes! -Sweet Dee',
+
+  'Oh, get a job? Just get a job? Why don’t I strap on my job helmet and squeeze down into a job cannon and fire off into job land, where jobs grow on jobbies?! -Charlie',
+
+  'Wanna go get sweaty in the bathroom? -Frank',
+
+  '[Singing] I got the Lord. I got the Lord. I got the good Lord, he’s goin’ down on me. -Charlie',
+
+  'They’re actors. They’re trying to create an illusion. In the “Lord of the Rings” movie, Ian McKellen plays a wizard. You think he goes home at night and shoots laser beams into his boyfriend’s asshole? I don’t think so, dude. Tom Cruise is a midget, alright, but he plays guys that are normal size in movies. -Mac',
+
+  'What is this word, spa? I feel like you’re starting to a say a word and you’re not finishing it. Are you trying to say spaghetti? Are you taking me for a spaghetti day? -Charlie',
+
+  'I’m not fat. I’m cultivating mass. -Fat Mac',
+
+  'Your mom doesn’t know dick! She’s a dumb, fat cow. And your sister, she’s a stupid little shit-mouthed bitch, isn’t she? -Sweet Dee',
+
+  '[Speaking like a radio host] TWO wars?! We’re in the midst of two wars all at once–Now the United States of America is engaged in both of these wars? -Dennis',
+
+  'Roxy, God bless you. You were a good whore. You serviced me like no other whore ever did. Not only my crank, but my heart. And…I’m gonna miss ya. Amen. -Frank',
+
+  'You should have seen how passionate he got when I showed him the dick flyer. -Charlie',
+
+  'It involves pulling up our bootstraps, oiling up a couple of asses and doing a little plowing of our own. Pow!…Not gay sex. -Mac',
+
+  'Cat in the wall, eh?! Okay, now you’re talking my language. -Charlie',
+
+  'I don’t appreciate being paraphrased. Now, I choose my words very deliberately. -Mac',
+
+  'We’re gonna get all in your face and point out your faults. -Sweet Dee',
+
+  '[Vietnamese music playing] This music sounds like whales raping each other. -Mac',
+
+  'Well, that’s gonna be a problem; I laser. It’s like a turtle’s shell down there. -Dennis',
+
+  'Oh, shit. Look at that door, dude. See that door right there? The one marked “pirate?” You think a pirate Charlie, in there? -Charlie',
+
+  'Fight Milk! The first alcoholic dairy based protein drink for bodyguards! -Mac',
+
+  'Later, boners. -Sweet Dee',
+
+  'I’m a…full-on rapist, you know? Uh, Africans, dyslexics, children, that sort of thing. -Charlie',
+
+  'Bite my bird. -Mac',
+
+  'Who am I supposed to vote for? Am I supposed to vote for the Democrat who’s gonna blast me in the ass or the Republican who’s blasting my ass? -Dennis',
+
+  'Paddy’s shotgun, bro. You get blasted in the mouth! -Mac',
+
+  'I eat stickers all the time, dude! -Charlie',
+
+  'I’m not gonna be buried in a grave. When I’m dead, just throw me in the trash. -Frank',
+
+  'Later, dudes. S you in your A’s, don’t wear a C and J all over your B’s. -Charlie',
+
+  'I hear the guy hangs dong and I’m very interested in seeing that. -Sweet Dee',
+
+  'THIS ISN’T OVER UNTIL I SAY IT’S OVER! -Charlie',
+
+  'If you’re in my room, you’re always being filmed. -Dennis',
+
+  'See, I’ve always got an A, B and C strike plan to get us out of any potentially life-threatening situation. -Mac',
+
+  'Cause if the girl said “no,” then the answer obviously is “no.” But the thing is, is she’s not gonna say “no.” She would never say “no,” because of the implication. -Dennis',
+
+  'Oh, you’re definitely gonna end up doing gay porn with this tiny little body of yours. -Sweet Dee',
+
+  'I got my Magnum condoms; I got my wad of hundreds. I’m ready to plow. -Frank',
+
+  'Can I stop you, though? You keep using this word “jabroni.” And…it’s awesome! -Charlie',
+
+  'Everybody’s dying, bitch. -Mac',
+
+  'Okay, listen. Listen, you remember that night at Dooley’s pool party? On that fine summer eve, when I did that double jackknife twist and blew everybody’s tits off. You remember that? And then I went down on Chrissy Orlando on the trampoline later that night? -Dennis',
+
+  '[Singing] I’m gonna rise up. I’m gonna kick a little ass. Gonna kick some ass in the U.S.A. Gonna climb a mountain. Gonna sew a flag. Gonna fly on an eagle. I’m gonna kick some butt. I’m gonna drive a big truck. I’m gonna rule this world. I’m gonna kick some ass. I’m gonna rise up. I’m gonna kick a little ass. ROCK, FLAG AND EAGLE! -Charlie',
+
+  'Flush that turd down the drain! -Frank',
+
+  'I browned out that evening. -Mac',
+
+  'See you later, Dirt Grub and Ronnie the Rat. Hope you suck each other’s rotten peckers until you get mouth cancer. -Sweet Dee',
+
+  'Boys are out tonight, huh? -Dennis',
+
+  'I will SMASH your face into a- into a jelly! -Charlie',
+
+  'So come on, you ol’ son of a gun, and let Buster do a line off your boner. -Buster the Jockey',
+
+  'Oh, you unzipped me! It’s all coming back! It’s all coming back; I hate you! It’s all coming back, you understand?! I DON’T LIKE IT! I DON’T LIKE TO THINK ABOUT IT! -Frank',
+
+  '[Singing] Dayman, fighter of the Nightman, champion of the sun, you’re a master of karate and friendship for everyone. Dayman! -Charlie and Dennis',
+
+  'I will eat your babies, bitch! -Sweet Dee',
+
+  'I shoulda popped my shirt off. Goddammit, really shoulda popped that shirt off. -Dennis',
+
+  'Wildcard, bitches! Yee-haw! -Charlie',
+
+  'Yeah, we both got punked, Frank. We both got punked. These kids these days, I’ll tell you what, they’re nothing like we used to be back when we were in fraternities. They have no respect for anybody. Okay? They’re like-they’re like stupid little goddamn savages. I mean, I came in there, right, and I was polite and I was nice to them. I was cordial. And they completely goddamn disrespected me, little IDIOTS! IDIOTS! I was completely respectful. They’re supposed to be my brothers, right? They’re my brothers? Nooo, no. That’s not fun. What they were doing wasn’t fun. They kept zapping us, and zapping us! IDIOTS! SAVAGES! IDIOTS! IDIOTS! -Dennis',
+
+  'Dennis, if I was looking for safe, I wouldn’t be sticking my dick through a wall. -Frank',
+
+  'Is your cat making TOO MUCH NOISE ALL THE TIME? -Charlie',
+
+  'Hey-o! What’s up, bitches! -Mac'
+]
