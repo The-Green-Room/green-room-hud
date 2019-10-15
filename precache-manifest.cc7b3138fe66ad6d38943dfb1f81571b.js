18a48932e8e4b335a3c46e8ed46eb022ef2032d5
Andrew Cash <asc010@uark.edu>
2019-10-15 16:16:14 -0500

    Updates

---
+++ precache-manifest.cc7b3138fe66ad6d38943dfb1f81571b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc42b0353bc2ceb42e297c1e3f8dca0b",
+    "url": "/hud/index.html"
+  },
+  {
+    "revision": "3d68daa65665b04c906b",
+    "url": "/hud/static/css/2.5ed8efde.chunk.css"
+  },
+  {
+    "revision": "ac3b2a62351f3f0f90bc",
+    "url": "/hud/static/css/main.b21d5843.chunk.css"
+  },
+  {
+    "revision": "3d68daa65665b04c906b",
+    "url": "/hud/static/js/2.d90bae82.chunk.js"
+  },
+  {
+    "revision": "ac3b2a62351f3f0f90bc",
+    "url": "/hud/static/js/main.cd6e124f.chunk.js"
+  },
+  {
+    "revision": "8ab8192c8afbf05cc8c4",
+    "url": "/hud/static/js/runtime-main.2426aa9e.js"
+  },
+  {
+    "revision": "791709baafb02a958510b80bb9b85db0",
+    "url": "/hud/static/media/calendar_config.791709ba.bin"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1afa805716fd4fb871808649f84587bf.js"
+  "/hud/precache-manifest.cc7b3138fe66ad6d38943dfb1f81571b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hud/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
