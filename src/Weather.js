e12cdde2c2645bbc28652476cbab81e3f83921c0
Andrew Cash <asc010@uark.edu>
2019-10-15 23:11:44 -0500

    Display current temp and city

--- src/Weather.js
+++ src/Weather.js
@@ -10,26 +10,27 @@ class Weather extends React.Component {
         // Default lat and long
         this.lat = 36.078193399999996;
         this.lon = -94.1901826;
-        this.apikey="2a8bf8ec10b65fdc9695467f42bef9b2"
+        this.apikey='2a8bf8ec10b65fdc9695467f42bef9b2'
+        this.units='imperial'
     }
     
     // TODO: Ask for current location and call api based on that
     // refer to this maybe: https://stackoverflow.com/questions/13194623/get-location-when-pages-loads
 
     componentDidMount() {
-        this.intervalID = setInterval(this.getWeather(this.lat, this.lon, this.apikey), 600000)
+        this.intervalID = setInterval(this.getWeather(this.lat, this.lon, this.apikey, this.units), 600000)
     }
 
     componentWillUnmount() {
         clearInterval(this.intervalID)
     }
 
-    getWeather(lat, lon, apikey) {
+    getWeather(lat, lon, apikey, units) {
         // this string should work but its not for some reason
         //'http://api.openweathermap.org/data/2.5/weather?lat=' + {lat} + '&lon=' + {lon} + '&APPID=' + {apikey}
 
         this.setState({ loading: true }, () => {
-            fetch('https://api.openweathermap.org/data/2.5/weather?lat=36.078193399999996&lon=-94.1901826&APPID=5e309b45fc1941bd17f5ec40b712220f&units=imperial')
+            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + apikey + '&units=' + units)
             .then(response =>  response.json())
             .then(data => this.setState({ 
                 loading: false,
@@ -41,7 +42,14 @@ class Weather extends React.Component {
     render() {
         return(
             <div className="forecast">
-                {this.state.loading ? <p>Loading...</p> : this.state.data.main.temp}
+                {this.state.loading ? <p>Loading...</p> : 
+                                    <h1>
+                                        It's currently {Math.trunc(this.state.data.main.temp)} degrees in {this.state.data.name}
+                                    </h1>
+                                    // <h5>
+                                    //     It's currently {this.state.data.main.temp} degrees in {this.state.data.name}
+                                    // </h5>
+                }                
             </div>
 
         )
