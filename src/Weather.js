63e8cdcd2013c838fe2c5074a3ebf4559e78edc1
Andrew Cash <asc010@uark.edu>
2019-10-15 19:08:14 -0500

    Attempt to add npm package to solve problem but its been unsuccessful

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactWeather from 'react-open-weather'
 
 class Weather extends React.Component {
     constructor() {
@@ -10,10 +11,13 @@ class Weather extends React.Component {
 
         // Default lat and long
         this.lat = 36.078193399999996;
-        this.long = -94.1901826;
+        this.lon = -94.1901826;
+        this.apikey="2a8bf8ec10b65fdc9695467f42bef9b2"
     }
     
     // TODO: Ask for current location and call api based on that
+    // refer to this maybe: https://stackoverflow.com/questions/13194623/get-location-when-pages-loads
+
     // componentDidMount() {
     //     var startPos;
         
@@ -25,12 +29,22 @@ class Weather extends React.Component {
     //     };
       
     //     navigator.geolocation.getCurrentPosition(geoSuccess);
+        
     // }
+
+    
 
     render() {
         return(
             <div>
-                <p>69 degrees</p>
+                <ReactWeather 
+                    forecast="5days" 
+                    unit="imperial"
+                    apikey={this.apikey}
+                    type="geo"
+                    lat={this.lat}
+                    lon={this.lon}
+                />
             </div>
         )
     }
