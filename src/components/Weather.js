f5fd35f66ca8d1e25c8fcf987ce25af6c9b10fd8
Andrew Cash <asc010@uark.edu>
2019-10-17 04:19:34 -0500

    Changed layout of weather and text

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -80,7 +80,6 @@ class Weather extends React.Component {
   }
 
   render() {
-    console.log(this.state)
     return(
       <div className="weather" >
         {this.state.loading ? <p>Loading...</p> : 
